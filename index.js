434f7d3f92625f0f896a29ea9952414e73f8924e
thienhoang <thient91@gmail.com>
2015-07-17 11:17:31 +0700

    update

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  	res.writeHead(200, {'Content-Type': 'text/plain'});
-  	res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
+var cool = require('cool-ascii-faces');
+var express = require('express');
+var app = express();
 
-console.log('Server running at http://127.0.0.1:1337/ ');
+app.set('port', (process.env.PORT || 5000));
+
+app.use(express.static(__dirname + '/public'));
+
+// views is directory for all template files
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+app.get('/', function(request, response) {
+  	response.render('pages/index')
+});
+
+app.get('/cool', function(request, response) {
+  	response.send(cool());
+});
+
+app.listen(app.get('port'), function() {
+  	console.log('Node app is running on port', app.get('port'));
+});
